4458cd006033ade2952fcb3b9e5fbe64dfdf2579
jcoder <847423815@qq.com>
2023-08-29 15:28:13 +0800

    init project commit localhost

---
+++ mybatis_test01/src/main/java/com/mybatis/dao/UserDao.java
@@ -0,0 +1,14 @@
+package com.mybatis.dao;
+
+import com.mybatis.pojo.User;
+
+import java.util.List;
+
+/**
+ * @Author j
+ * @Date 2023/8/29 14:26
+ * @Version 1.0
+ */
+public interface UserDao {
+    List<User> findAll();
+}
